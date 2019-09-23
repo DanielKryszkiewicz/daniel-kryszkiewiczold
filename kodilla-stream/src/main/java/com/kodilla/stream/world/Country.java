54f128b673aa5bb3e74913e7eddb3685c31161a9
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2019-09-23 22:14:24 +0200

    game

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+
+public class Country {
+
+    public BigDecimal getPeopleQuantity(){
+        BigDecimal peopleQuantity = new BigDecimal("");
+
+        return peopleQuantity;
+
+        }
+    }
+}
+
+
