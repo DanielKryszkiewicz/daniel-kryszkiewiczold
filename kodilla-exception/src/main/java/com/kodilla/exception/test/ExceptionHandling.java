a2f51eb04a17b1f7e7136a8041fbc9d728cd10bf
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2019-11-12 17:37:30 +0100

    8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling extends SecondChallenge {
+
+    public void runExceptionhandling() {
+
+        SecondChallenge check = new SecondChallenge();
+
+        try {
+
+            System.out.println(check.probablyIWillThrowException(2,1));
+
+        } catch (Exception e){
+
+            System.out.println(e);
+
+        } finally {
+            System.out.println("Finish");
+        }
+    }
+}
+}
+
