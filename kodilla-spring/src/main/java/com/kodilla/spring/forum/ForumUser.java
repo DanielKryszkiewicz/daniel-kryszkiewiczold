27d5bf0b0bb6d4967cba51f3542334a87767025a
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2019-11-14 12:59:43 +0100

    14.1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String userName;
+
+    public ForumUser(){
+        userName = "John Smith";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
+
+
+
+
+
+
