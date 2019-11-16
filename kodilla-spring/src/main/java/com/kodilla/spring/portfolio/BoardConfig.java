a482850386d765a4cf59e8b017be14a0f3baa459
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2019-11-16 16:27:23 +0100

    14.4

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    Board board;
 
     @Bean
     public Board getBoard(){
